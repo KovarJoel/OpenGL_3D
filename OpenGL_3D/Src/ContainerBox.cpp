eb78f1fc0ddea3cb2aa73321f3b8804825a680a1
KovarJoel <joel.m.kovar@gmail.com>
2023-02-03 22:17:00 +0100

    Played a bit with it to better understand it

--- OpenGL_3D/Src/ContainerBox.cpp
+++ OpenGL_3D/Src/ContainerBox.cpp
@@ -2,7 +2,7 @@
 
 ContainerBox::ContainerBox()
 {
-	vert.reserve(40);
+	vert.reserve(120);
 	setSize(1.0f);
 
 	VAO = new VertexArray(vert.size(), &vert[0], {3, 2}, indices);
@@ -10,11 +10,16 @@ ContainerBox::ContainerBox()
 	texture1 = new Texture("Src\\Textures\\containerBox.jpg");
 	texture2 = new Texture("Src\\Textures\\awesomeFace.png");
 	
-	shaderProgram->use();
+	shaderProgram->bind();
 	texture1->bind();
 	shaderProgram->setIntUniform("texture1", { texture1->getHandle() - 1 });
 	texture2->bind();
 	shaderProgram->setIntUniform("texture2", { texture2->getHandle() - 1});
+
+	view = glm::mat4(1.0f);
+	view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+	projection = glm::mat4(1.0f);
+	projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 }
 
 ContainerBox::~ContainerBox()
@@ -28,21 +33,17 @@ ContainerBox::~ContainerBox()
 void ContainerBox::render()
 {
 	model = glm::mat4(1.0f);
-	model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
-	view = glm::mat4(1.0f);
-	view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-	projection = glm::mat4(1.0f);
-	projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+	model = glm::rotate(model, 2.0f * (float)glfwGetTime(), glm::vec3(1.0f, 1.5f, 0.0f));
 
 	texture1->bind();
 	texture2->bind();
-	shaderProgram->use();
-	shaderProgram->setMatrix4Uniform("model", { model });
-	shaderProgram->setMatrix4Uniform("view", { view });
-	shaderProgram->setMatrix4Uniform("projection", { projection });
+	shaderProgram->bind();
+	shaderProgram->setMatrix4Uniform("model", model);
+	shaderProgram->setMatrix4Uniform("view", view);
+	shaderProgram->setMatrix4Uniform("projection", projection);
 	VAO->bind();
-	glDrawElements(GL_TRIANGLES, vert.size(), GL_UNSIGNED_INT, 0);
-	//glDrawArrays(GL_TRIANGLES, 0, 36);
+	glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+	//glDrawArrays(GL_TRIANGLES, 0, 120);
 	VAO->unbind();
 }
 
@@ -54,16 +55,38 @@ void ContainerBox::handleEvents(GLFWwindow* window)
 void ContainerBox::setSize(float length)
 {
 	float size = length / 2.0f;
-
+	
 	vert = {
-		-size, +size, +size, 0.0f, 1.0f,	// 0
-		+size, +size, +size, 1.0f, 1.0f,	// 1
-		+size, -size, +size, 1.0f, 0.0f,	// 2
-		-size, -size, +size, 0.0f, 0.0f,	// 3
-											// 
-		-size, +size, -size, 0.0f, 1.0f,	// 4
-		+size, +size, -size, 1.0f, 1.0f,	// 5
-		+size, -size, -size, 1.0f, 0.0f,	// 6
-		-size, -size, -size, 0.0f, 0.0f		// 7
+		//	x,	   y,	  z,	w,	  h,
+		// front
+		-size, +size, +size, 0.0f, 1.0f,	// 00 top left
+		+size, +size, +size, 1.0f, 1.0f,	// 01 top right
+		+size, -size, +size, 1.0f, 0.0f,	// 02 bottom  right
+		-size, -size, +size, 0.0f, 0.0f,	// 03 bottom left
+		// right
+		+size, +size, +size, 0.0f, 1.0f,	// 04 top left
+		+size, +size, -size, 1.0f, 1.0f,	// 05 top right
+		+size, -size, -size, 1.0f, 0.0f,	// 06 bottom  right
+		+size, -size, +size, 0.0f, 0.0f,	// 07 bottom left
+		// back
+		-size, +size, -size, 0.0f, 1.0f,	// 08 top left
+		+size, +size, -size, 1.0f, 1.0f,	// 09 top right
+		+size, -size, -size, 1.0f, 0.0f,	// 10 bottom  right
+		-size, -size, -size, 0.0f, 0.0f,	// 11 bottom left
+		// left
+		-size, +size, -size, 0.0f, 1.0f,	// 12 top left
+		-size, +size, +size, 1.0f, 1.0f,	// 13 top right
+		-size, -size, +size, 1.0f, 0.0f,	// 14 bottom  right
+		-size, -size, -size, 0.0f, 0.0f,	// 15 bottom left
+		// top
+		-size, +size, -size, 0.0f, 1.0f,	// 16 back left
+		+size, +size, -size, 1.0f, 1.0f,	// 17 back right
+		+size, +size, +size, 1.0f, 0.0f,	// 18 front right
+		-size, +size, +size, 0.0f, 0.0f,	// 19 front left
+		// bottom
+		-size, -size, -size, 0.0f, 1.0f,	// 20 back left
+		+size, -size, -size, 1.0f, 1.0f,	// 21 back right
+		+size, -size, +size, 1.0f, 0.0f,	// 22 front right
+		-size, -size, +size, 0.0f, 0.0f,	// 23 front left
 	};
 }
