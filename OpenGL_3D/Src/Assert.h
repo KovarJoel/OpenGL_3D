2d1cfb1494cc6390777dab73a6fe87596a9c4987
KovarJoel <joel.m.kovar@gmail.com>
2023-02-11 16:57:10 +0100

    Made the "Assert" constructor private.

--- OpenGL_3D/Src/Assert.h
+++ OpenGL_3D/Src/Assert.h
@@ -9,6 +9,11 @@ Assert::assertGL(__FILE__, __LINE__, __FUNCTION__, #x)
 
 class Assert
 {
+private:
+	Assert() = default;
+	Assert(const Assert& original) = default;
+	~Assert() = default;
+
 public:
 	static void assertMsg(const std::string& file, const int line, const std::string& function, const std::string& message);
 	static void assertGL(const std::string& file, const int line, const std::string& function, const std::string& code);
